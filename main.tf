8cf129a78ec1b840b038bd4566725f2f5ed5bd51
olalekanolanrewaju <78469928+olalekanolanrewaju@users.noreply.github.com>
2023-08-10 14:24:17 +0100

    Initial commit
    Terraform SDK (Azure Code Base)

---
+++ main.tf
@@ -0,0 +1,27 @@
+/*
+
+The following links provide the documentation for the new blocks used
+in this terraform configuration file
+
+1. azurerm_resource_group - https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
+
+*/
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.8.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  
+  features {}
+}
+
+resource "azurerm_resource_group" "appgrp" {
+  name     = "app-grp"
+  location = "North Europe"
+}
